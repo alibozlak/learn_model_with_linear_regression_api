fbd7e61b2a286c6e69910865eb2888be6514bff8
Ali Bozlak <bozlak20aliihsan@gmail.com>
2026-08-20 14:16:23 +0300

    bash script file docker image version has been changed.

--- start_with_docker.sh
+++ start_with_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -p 3000:3000 --name=linear_regression bozlak/linear_regression_api:0.3
+docker run --rm -p 3000:3000 --name=linear_regression bozlak/linear_regression_api:0.4
 
